080346c2041cce4baa839cc4f0c720e8d6ec7dd2
DESKTOP-ECIK0QF\luthi <luthita@naver.com>
2021-10-31 18:46:21 +0900

    브루트포스

--- src/lesson11/No1_2798.java
+++ src/lesson11/No1_2798.java
@@ -2,8 +2,36 @@ package lesson11;
 
 import java.util.Scanner;
 
+// ∫Ì∑¢¿Ë
 public class No1_2798 {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int m = scan.nextInt();
+		int[] card = new int[n];
+		int result = 0;
+		for(int i = 0; i < n; i++) {
+			card[i] = scan.nextInt();
+		}
 
-
+		outline : for(int i =0; i < n-2; i++) {
+			for(int j = i+1; j < n-1; j++) {
+				for(int k = j+1; k < n; k++) {
+					int temp = card[i] + card[j] + card[k];
+					
+					if(temp <= m) {
+						result = Math.max(result, temp);
+					}
+					if(result == m) {
+						break outline;
+					}
+				}
+			}
+		}
+		System.out.println(result);
+	}
 }
--- src/lesson11/No2_2231.java
+++ src/lesson11/No2_2231.java
@@ -2,5 +2,9 @@ package lesson11;
 
 public class No2_2231 {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
